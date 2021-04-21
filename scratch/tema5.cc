78aec1f770d3d96b0b2d37a6b7c2110365fdc159
kirschovapetra <kirschovapetra@gmail.com>
2021-04-21 13:07:15 +0200

    mini upravy

--- scratch/tema5.cc
+++ scratch/tema5.cc
@@ -1,12 +1,34 @@
-#include "ns3/core-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/network-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/csma-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/yans-wifi-helper.h"
-#include "ns3/ssid.h"
+#include <bits/stdint-uintn.h>
+#include <ns3/application-container.h>
+#include <ns3/boolean.h>
+#include <ns3/command-line.h>
+#include <ns3/csma-helper.h>
+#include <ns3/data-rate.h>
+#include <ns3/internet-stack-helper.h>
+#include <ns3/ipv4-address-helper.h>
+#include <ns3/ipv4-interface-container.h>
+#include <ns3/log.h>
+#include <ns3/mobility-helper.h>
+#include <ns3/names.h>
+#include <ns3/net-device-container.h>
+#include <ns3/node.h>
+#include <ns3/node-container.h>
+#include <ns3/nstime.h>
+#include <ns3/object.h>
+#include <ns3/position-allocator.h>
+#include <ns3/ptr.h>
+#include <ns3/simulator.h>
+#include <ns3/ssid.h>
+#include <ns3/uinteger.h>
+#include <ns3/v4ping-helper.h>
+#include <ns3/vector.h>
+#include <ns3/waypoint.h>
+#include <ns3/waypoint-mobility-model.h>
+#include <ns3/wifi-helper.h>
+#include <ns3/wifi-mac-helper.h>
+#include <ns3/yans-wifi-helper.h>
+#include <string>
+#include <vector>
 
 using namespace ns3;
 using namespace std;
@@ -65,7 +87,9 @@ vector<Vector> zastavkyConstPosModel(MobilityHelper& mobility, NodeContainer& no
 }
 
 
-void zastavkyCsmaConnections(CsmaHelper& csma) {
+vector<NetDeviceContainer> zastavkyCsmaConnections(CsmaHelper& csma) {
+
+  vector<NetDeviceContainer> nic_zastavky = {};
 
   // spojenia medzi dvojicami zastavok
   vector<NodeContainer> spojenia = { NodeContainer(NodeContainer("A"),NodeContainer("B")),
@@ -83,8 +107,20 @@ void zastavkyCsmaConnections(CsmaHelper& csma) {
 
 //   instalovanie net devices
   for (const auto& spoj : spojenia) {
-     NetDeviceContainer container = csma.Install(spoj);
+      nic_zastavky.push_back(csma.Install(spoj));
   }
+
+  return nic_zastavky;
+}
+
+
+void setWaypoints(vector<Vector> pozicie_zastavok, Ptr<Node> elektricka, double interval, vector<int> cesta) {
+  Ptr<WaypointMobilityModel> model = DynamicCast<WaypointMobilityModel>( elektricka->GetObject<MobilityModel>() );
+
+  for (int i = 0; i < cesta.size(); i++) {
+      model->AddWaypoint (Waypoint(Seconds(interval*i), pozicie_zastavok[cesta[i]]));
+  }
+
 }
 
 
@@ -96,8 +132,7 @@ void elektrickyWaypointModel(MobilityHelper& mobility, NodeContainer& nody_elekt
    *      * nastavit mobility helperu dany model a nainstalovat jednu elektricku
    */
 
-  mobility.SetMobilityModel("ns3::WaypointMobilityModel",
-                            "LazyNotify",BooleanValue(true));
+  mobility.SetMobilityModel("ns3::WaypointMobilityModel", "LazyNotify",BooleanValue(true));
   mobility.Install(nody_elektricky);
 
   // init pozicie elektriciek
@@ -111,47 +146,18 @@ void elektrickyWaypointModel(MobilityHelper& mobility, NodeContainer& nody_elekt
   for( auto& pozicia: pozicieElektriciekVecList) {
       pozicieElektriciekAlloc -> Add(pozicia);
   }
+  mobility.SetPositionAllocator(pozicieElektriciekAlloc);
 
   // waypoint model
-//  ObjectFactory mobilityFactory;
-//  mobilityFactory.SetTypeId ("ns3::WaypointMobilityModel");
-//  mobilityFactory.Set ("LazyNotify", BooleanValue (true));
-//
-//  Ptr<MobilityModel> model = mobilityFactory.Create()->GetObject<MobilityModel>();
-//  Ptr<WaypointMobilityModel> mob = model->GetObject<WaypointMobilityModel>();
-
 
   /* elektricka 1: A (0) -> B (1) -> C (2) -> D (3) -> H (7)-> I (8)*/
-
-  vector<int> elektricka1_cesta{0,1,2,3,7,8};
-  Ptr<WaypointMobilityModel> elektricka1_waypointModel =
-      DynamicCast<WaypointMobilityModel>( nody_elektricky.Get(0)->GetObject<MobilityModel>() );
-
-  for (int i = 0; i < elektricka1_cesta.size(); i++) {
-      elektricka1_waypointModel->AddWaypoint (Waypoint(Seconds(interval*i), pozicie_zastavok[elektricka1_cesta[i]]));
-  }
-
+  setWaypoints(pozicie_zastavok,nody_elektricky.Get(0), interval, {0,1,2,3,7,8});
 
   /* elektricka 2: E (4) -> F (5) -> G (6) -> J (9) */
-
-  vector<int> elektricka2_cesta{4,5,6,9};
-  Ptr<WaypointMobilityModel> elektricka2_waypointModel =
-      DynamicCast<WaypointMobilityModel>( nody_elektricky.Get(1)->GetObject<MobilityModel>() );
-
-  for (int i = 0; i < elektricka2_cesta.size(); i++) {
-      elektricka2_waypointModel->AddWaypoint (Waypoint(Seconds(interval*i), pozicie_zastavok[elektricka2_cesta[i]]));
-  }
-
+  setWaypoints(pozicie_zastavok,nody_elektricky.Get(1), interval, {4,5,6,9});
 
   /* elektricka 3: J (9) -> G (6) -> H (5) -> F (2) -> B (1) -> A (0) */
-
-  vector<int> elektricka3_cesta{9,6,5,2,1, 0};
-  Ptr<WaypointMobilityModel> elektricka3_waypointModel =
-      DynamicCast<WaypointMobilityModel>( nody_elektricky.Get(2)->GetObject<MobilityModel>() );
-
-  for (int i = 0; i < elektricka3_cesta.size(); i++) {
-      elektricka3_waypointModel->AddWaypoint (Waypoint(Seconds(interval*i), pozicie_zastavok[elektricka3_cesta[i]]));
-  }
+  setWaypoints(pozicie_zastavok,nody_elektricky.Get(2), interval, {9,6,5,2,1,0});
 
 }
 
@@ -198,57 +204,54 @@ int main(int argc, char *argv[]) {
     elektrickyWaypointModel(mobility,nody_elektricky, pozicie_zastavok, 5.0);
 
 
-//L2
-
-    // L2 -- nastavenia NIC a kanalov
+// L2 -- nastavenia NIC a kanalov
 
     // CSMA spojenia medzi zastavkami
     CsmaHelper csma;
     csma.SetChannelAttribute ("DataRate", DataRateValue (5000000));
     csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
-    zastavkyCsmaConnections(csma);
+    vector<NetDeviceContainer> nic_zastavky_all = zastavkyCsmaConnections(csma);
 
 
-//    // wi-fi channel
-//    YansWifiChannelHelper wChannel = YansWifiChannelHelper::Default ();
-//    YansWifiPhyHelper phy;
-//    phy.SetChannel (wChannel.Create ());
-//    WifiHelper wifiHelper;
-//    wifiHelper.SetRemoteStationManager ("ns3::AarfWifiManager");
-//    Ssid ssid = Ssid ("eduroam");
-//
-//    // mac
-//    WifiMacHelper mac;
-//    mac.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (ssid), "ActiveProbing", BooleanValue (false));
-//    NetDeviceContainer nic_elektricky;
-//    nic_elektricky = wifiHelper.Install (phy, mac, nody_elektriciek);
-//    mac.SetType ("ns3::ApWifiMac", "Ssid", SsidValue (ssid));
-//    NetDeviceContainer nicWap;
-//    nicWap = wifiHelper.Install (phy, mac, accessPoint); budeme mat nejaky access point?
+    // wi-fi channel - elektricky
+    YansWifiChannelHelper wChannel = YansWifiChannelHelper::Default ();
+    YansWifiPhyHelper phy;
+    phy.SetChannel (wChannel.Create ());
+    WifiHelper wifiHelper;
+    wifiHelper.SetRemoteStationManager ("ns3::AarfWifiManager");
+    Ssid ssid = Ssid ("imhd");
+
+    // mac - elektricky
+    WifiMacHelper mac;
+    mac.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (ssid), "ActiveProbing", BooleanValue (false));
+    NetDeviceContainer nic_elektricky;
+    nic_elektricky = wifiHelper.Install (phy, mac, nody_elektricky);
+    mac.SetType ("ns3::ApWifiMac", "Ssid", SsidValue (ssid));
+    NetDeviceContainer nicWap;
+//    nicWap = wifiHelper.Install (phy, mac, accessPoint); //budeme mat nejaky access point?
 
 
 // L3 -- IP
     InternetStackHelper stack;
     stack.InstallAll();
     Ipv4AddressHelper address;
-    // vytvorenie subnetu/podsiete s IP 10.2.1.0 a maskou 255.255.255.0
-    // TODO: vytvorit jednu siet pre zastavky a jednu pre elektricky?
+
     // ZASTAVKY:
-//    address.SetBase("10.2.1.0", "255.255.255.0");
-//    Ipv4InterfaceContainer networkSContainer = address.Assign(nic_zastavky);
+    address.SetBase("10.2.1.0", "255.255.255.0"); // vytvorenie subnetu/podsiete s IP 10.2.1.0 a maskou 255.255.255.0
+    for (auto& nic_zastavky: nic_zastavky_all){
+        Ipv4InterfaceContainer zastavky_networkContainer = address.Assign(nic_zastavky);
+    }
+
 //    // ELEKTRICKY:
-//    address.SetBase("10.2.2.0", "255.255.255.0");
-//    Ipv4InterfaceContainer networkWContainer = address.Assign(nic_elektricky);
-//    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    address.SetBase("10.2.2.0", "255.255.255.0"); // subnet 10.2.2.0 maska 255.255.255.0
+    Ipv4InterfaceContainer elektricky_networkContainer = address.Assign(nic_elektricky);
+
+//    Ipv4GlobalRoutingHelper::PopulateRoutingTables (); // toto hadze error
 
 
-//L4 TODO
+//L4 - L7 aplikacna vrstva
 
-//L5 TODO
 
-//L6 TODO
-
-//L7
     // TODO asi radsej tcp - TCP lebo zarucuje ze sa spravy dorucia v poriadku
 //    UdpEchoServerHelper echoServer(9);
 //
